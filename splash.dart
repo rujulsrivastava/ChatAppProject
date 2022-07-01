42f5c72568984b73c0e6bfb7241c3f3aed7688f1
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-01 18:31:36 +0530

    Update splash.dart

--- splash.dart
+++ splash.dart
@@ -1,51 +1,68 @@
-import 'dart:async';
-
+import 'package:chat_app_project/firebase_services/auth.dart';
+import 'package:chat_app_project/pages/login.dart';
+import 'package:chat_app_project/pages/signup.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
-import 'login.dart';
-
-class Splash extends StatefulWidget {
+class Splash extends StatelessWidget {
   const Splash({Key? key}) : super(key: key);
 
-  @override
-  State<Splash> createState() => _SplashState();
-}
 
-class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    Timer(
-        const Duration(seconds: 3),
-        () => Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (BuildContext context) => const Login())));
-
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFF00CCFF),
-            Color(0xFF3366FF),
-          ],
-          begin: FractionalOffset(0.0, 0.0),
-          end: FractionalOffset(1.0, 0.0),
-          stops: [0.0, 1.0],
-          tileMode: TileMode.clamp,
-        ),),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-          child: Container(
-            height: MediaQuery.of(context).size.height/2,
-            // width: MediaQuery.of(context).size.width/3,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fitWidth,
-                image: AssetImage('assets/bird.png'),
-              ),
+    double width = MediaQuery.of(context).size.width;
+    return Material(
+      color: const Color(0xFF274CE0),
+      child: Padding(
+        padding: EdgeInsets.all(width/10),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text("Welcome", style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.w600),),
+                SizedBox(height: 50,),
+                Text("Manage whatever whatever", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w300),),
+                SizedBox(height: 15,),
+                Text("Some more long, big text", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),),
+              ],
             ),
-            child: const Text("SPLASH", style: TextStyle(fontSize: 50, fontWeight: FontWeight.w300, color: Colors.white),),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 25),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    MaterialButton(
+                      height: 40,
+                      minWidth: MediaQuery.of(context).size.width-(2*MediaQuery.of(context).size.width/10),
+                      onPressed: () {googleSignIn(context);},
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
 
-          )),
+                      textColor: const Color(0xFF274CE0),
+                      child: const Text("Sign in with Google"),
+                    ),
+                    const SizedBox(height: 5,),
+                    MaterialButton(
+                      height: 40,
+                      minWidth: MediaQuery.of(context).size.width-(2*MediaQuery.of(context).size.width/10),
+                      onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUp()));},
+                      color: const Color(0xFF274CE0),
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(side: const BorderSide(color: Colors.white70, width: 1), borderRadius: BorderRadius.circular(7)),
+                      child: const Text("Create an account"),
+                    ),
+                    const SizedBox(height: 14,),
+                    GestureDetector(
+                      onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const Login()));},
+                      child: const Text("Already have an account? Sign in",
+                        style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.w400),),
+                    )
+                  ],
+                ),
+              ),
+            )
+        ]),
       ),
     );
   }
