bdb58a2bb86b66ac748d5d4b1cc93563ee2db3b2
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-04 07:44:19 +0530

    Update database.dart

--- database.dart
+++ database.dart
@@ -1,13 +1,12 @@
+import 'package:chat_app_project/customs/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 getGroupList() async {
-  // return snapshots();
   var snapshot = await FirebaseFirestore.instance.collection("users").get();
   return snapshot.docs;
 }
 
 void createGroup(String groupName, String userName) async {
-  // final String groupName;
-  // final String userName;
   FirebaseFirestore.instance.collection("chatgroups").add(
       {
         "groupName"    : groupName,
@@ -17,20 +16,39 @@ void createGroup(String groupName, String userName) async {
   });
 }
 
-void createUser(String email, String userName, String password) {
-  FirebaseFirestore.instance.collection("users").add(
-    {
-      "userName" : userName,
-      "email" : email,
-      "password" : password,
-    }
-  );
+Future<void> createUser(MyUser user) async {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  try {
+    await firebaseFirestore.collection("users").doc(auth.currentUser!.uid).set(
+      {
+        "uid" : auth.currentUser!.uid,
+        "userName" : user.userName,
+        "phone" : user.phone,
+        "password" : user.password,
+        "email" : user.email,
+        "dateOfCreation" : user.dateOfCreation,
+      }
+    );
+  } catch (e) {print(e.toString());}
 }
 
-// getUserData()
-//
-// Future updateProfile({String? userName, String? email, String? phone}) async {
-//   var collection = FirebaseFirestore.instance.collection('users');
-//   collection
-//   .doc()
-// }
+updateData(String userName, String? email, String phone) async {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  try {
+    await auth.currentUser!.updateDisplayName(userName);
+    await firebaseFirestore.collection("users").doc(auth.currentUser!.uid).set(
+        {
+          "userName" : userName,
+          "phone" : phone,
+          "email" : email,
+        }
+    );
+  } catch (e) {print(e.toString());}
+}
+
+Future<Map<String, dynamic>?> getUserData(String uid) async  {
+  var snapshot = await FirebaseFirestore.instance.collection("users").doc(uid).get();
+  return snapshot.data();
+}
