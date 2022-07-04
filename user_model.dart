3a305d69e197c3bda6d2d77580939cceac0ce136
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-04 07:45:45 +0530

    Add files via upload

---
+++ user_model.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MyUser{
+  late String uid;
+  late String? phone;
+  late String userName;
+  late String? password;
+  late String? email;
+  late Timestamp dateOfCreation;
+
+  MyUser({required this.userName, this.password, this.phone, this.email, required this.dateOfCreation});
+
+  MyUser.fromMap(Map<String, dynamic> mapData) {
+    uid = mapData["uid"];
+    userName = mapData["userName"];
+    email = mapData["email"];
+    phone = mapData["phone"];
+    dateOfCreation = mapData["dateOfCreation"];
+  }
+}
