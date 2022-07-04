c4f9ec8a2ab188e1c8d3e36188f5414641f4210e
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-04 18:10:24 +0530

    Update home.dart

--- home.dart
+++ home.dart
@@ -8,8 +8,8 @@ import 'package:chat_app_project/firebase_services/messaging.dart';
 import 'package:chat_app_project/customs/group_tile.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key, required this.username}) : super(key: key);
-  final String username;
+  const Home({Key? key, required this.userName}) : super(key: key);
+  final String userName;
 
   @override
   State<Home> createState() => _HomeState();
@@ -17,13 +17,20 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController searchContact = TextEditingController();
-  Messaging messaging = Messaging();
   static FirebaseAuth auth = FirebaseAuth.instance;
-  // String userName = auth.currentUser!.displayName!=null ? auth.currentUser!.displayName : "randomusername";
+  late Stream _groups;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserJoinedGroups();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color(0xFF274CE0),
         leading: Builder(
             builder: (context) => IconButton(
                   icon: const Icon(Icons.menu),
@@ -32,18 +39,19 @@ class _HomeState extends State<Home> {
                   },
                   // tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
                 )),
-        title: Text("Hi ${widget.username}"),
+        title: Text("Hi ${widget.userName}"),
         centerTitle: true,
         actions: [
           IconButton(
               onPressed: () {
-                googleSignOut(context);
+                signOut(context);
               },
               icon: const Icon(Icons.logout)),
           IconButton(
               onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const Profile()));
-            },
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Profile()));
+              },
               icon: const Icon(Icons.person)),
         ],
       ),
@@ -65,11 +73,24 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: renderGroups(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+
+          SingleChildScrollView(
+              //height: MediaQuery.of(context).size.height / 4,
+              child: renderUserGroups()),
+
+          SingleChildScrollView(
+              // height: MediaQuery.of(context).size.height / 4,
+              child: renderAllGroups()),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () { createGroupDialog(); },
-      child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            createGroupDialog();
+          },
+          child: const Icon(Icons.add)),
     );
   }
 
@@ -78,23 +99,86 @@ class _HomeState extends State<Home> {
     AlertDialog alert = AlertDialog(
       title: const Text("Create a group chat!"),
       content: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          TextField(controller: controller, decoration: const InputDecoration(hintText: "Add a group name"),),
+          TextField(
+            controller: controller,
+            decoration: const InputDecoration(hintText: "Add a group name"),
+          ),
         ],
       ),
       actions: [
-        IconButton(onPressed: () {
-          createGroup(controller.text, auth.currentUser!.displayName!);
-          Navigator.of(context).pop();
-
-          },
+        IconButton(
+            onPressed: () {
+              createGroup(controller.text, auth.currentUser!.displayName!,
+                  auth.currentUser!.uid);
+              Navigator.of(context).pop();
+            },
             icon: const Icon(Icons.check)),
       ],
     );
-    showDialog(context: context, builder: (context) => alert) ;
+    showDialog(context: context, builder: (context) => alert);
   }
 
-  Widget renderGroups() {
+  Widget renderUserGroups() {
+    return StreamBuilder(
+      stream: _groups,
+      builder: (context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data['usergroups'] != null) {
+            print("length is");
+            print(snapshot.data['usergroups'].length);
+            if (snapshot.data['usergroups'].length != 0) {
+              return ListView.builder(
+                  itemCount: snapshot.data['usergroups'].length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    int reqIndex =
+                        snapshot.data['usergroups'].length - index - 1;
+                    return GroupTile(
+                        userName: auth.currentUser!.displayName!,
+                        groupId: _destructureId(
+                            snapshot.data['usergroups'][reqIndex]),
+                        groupName: _destructureName(
+                            snapshot.data['usergroups'][reqIndex]));
+                  });
+            } else {
+              return const Center(
+                  child: Text(
+                      "You haven't joined any groups. Join or create onee!"));
+            }
+          } else {
+            return const Center(
+                child:
+                    Text("You haven't joined any groups. Join or create one now!"));
+          }
+        } else {
+          return const Center(child: Text("unable to fetch data"));
+        }
+      },
+    );
+  }
+
+  String _destructureId(String res) {
+    // print(res.substring(0, res.indexOf('_')));
+    return res.substring(0, res.indexOf('_'));
+  }
+
+  String _destructureName(String res) {
+    // print(res.substring(res.indexOf('_') + 1));
+    return res.substring(res.indexOf('_') + 1);
+  }
+
+  _getUserJoinedGroups() async {
+    getUserGroups(auth.currentUser!.uid).then((snapshots) {
+      // print(snapshots);
+      setState(() {
+        _groups = snapshots;
+      });
+    });
+  }
+
+  Widget renderAllGroups() {
     return StreamBuilder(
       stream: FirebaseFirestore.instance.collection('chatgroups').snapshots(),
       builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -112,7 +196,8 @@ class _HomeState extends State<Home> {
                     snapshot.data?.docs[index] as DocumentSnapshot<Object?>;
                 return GroupTile(
                   groupName: ds["groupName"],
-                  // userName: auth.currentUser!.displayName!,
+                  groupId: ds.id,
+                  userName: ds["createdBy"],
                 );
               });
         }
