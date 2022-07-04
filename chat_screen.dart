592d3e5c17c98e693d7bb8e9bcf1526c3ad45173
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-04 18:14:56 +0530

    Update chat_screen.dart

--- chat_screen.dart
+++ chat_screen.dart
@@ -1,14 +1,158 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({Key? key}) : super(key: key);
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chat_app_project/firebase_services/database.dart';
+import 'package:chat_app_project/customs/message_tile.dart';
+
+import '../firebase_services/messaging.dart';
+
+class ChatPage extends StatefulWidget {
+
+  final String groupId;
+  final String userName;
+  final String groupName;
+
+  ChatPage({
+    required this.groupId,
+    required this.userName,
+    required this.groupName
+  });
 
   @override
-  State<ChatScreen> createState() => _ChatScreenState();
+  _ChatPageState createState() => _ChatPageState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatPageState extends State<ChatPage> {
+
+  late Stream<QuerySnapshot<Map<String, dynamic>>> _chats;
+  TextEditingController messageEditingController = TextEditingController();
+
+  Widget _chatMessages(){
+    return StreamBuilder(
+      stream: _chats,
+      builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot){
+        return snapshot.hasData ?  ListView.builder(
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (context, index){
+              return MessageTile(
+                message: snapshot.data?.docs[index].data()["message"],
+                sender: snapshot.data?.docs[index].data()["sender"],
+                sentByMe: FirebaseAuth.instance.currentUser!.displayName == snapshot.data?.docs[index].data()["sender"],
+                time: snapshot.data?.docs[index].data()["time"]
+              );
+            }
+        )
+            :
+        Container();
+      },
+    );
+  }
+
+  _sendMessage() {
+    if (messageEditingController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "message": messageEditingController.text,
+        "sender": FirebaseAuth.instance.currentUser!.displayName,
+        "time" : Timestamp.now()
+      };
+
+      sendMessage(widget.groupId, chatMessageMap);
+
+      setState(() {
+        messageEditingController.text = "";
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getChats(widget.groupId).then((val) {
+      // print(val);
+      setState(() {
+        _chats = val;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.groupName, style: const TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF043F4A),
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            _chatMessages(),
+            // Container(),
+            Container(
+              alignment: Alignment.bottomCenter,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                color: Colors.grey[700],
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: messageEditingController,
+                        style: const TextStyle(
+                            color: Colors.white
+                        ),
+                        decoration: const InputDecoration(
+                            hintText: "Send a message ...",
+                            hintStyle: TextStyle(
+                              color: Colors.white38,
+                              fontSize: 16,
+                            ),
+                            border: InputBorder.none
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(width: 12.0),
+
+                    GestureDetector(
+                      onTap: () {
+                        _sendMessage();
+                      },
+                      child: Container(
+                        height: 50.0,
+                        width: 50.0,
+                        decoration: BoxDecoration(
+                            color: const Color(0xFF043F4A),
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: const Center(child: Icon(Icons.send, color: Colors.white)),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        _sendMessage();
+                      },
+                      child: Container(
+                        height: 50.0,
+                        width: 50.0,
+                        decoration: BoxDecoration(
+                            color: const Color(0xFF043F4A),
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: const Center(child: Icon(Icons.send, color: Colors.white)),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
