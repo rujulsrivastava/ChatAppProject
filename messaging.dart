519dcd91646e542783d6c521e4f803fc4055675a
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-11 08:07:38 +0530

    Update messaging.dart

--- messaging.dart
+++ messaging.dart
@@ -2,9 +2,26 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:chat_app_project/customs/message_template.dart';
 
-sendMessage(String groupId, chatMessageData) {
+sendMessageInGroup(String groupId, chatMessageData) {
   FirebaseFirestore.instance.collection('chatgroups').doc(groupId).collection('messages').add(chatMessageData);
   FirebaseFirestore.instance.collection('chatgroups').doc(groupId).update({
+    'recentMessage': chatMessageData['message'],
+    'recentMessageSender': chatMessageData['sender'],
+    'recentMessageTime': chatMessageData['time']
+  });
+}
+
+sendMessageInChat(String senderID, String receiverID, String docID, chatMessageData) {
+  CollectionReference personalchats = FirebaseFirestore.instance.collection('personalchats');
+  CollectionReference users = FirebaseFirestore.instance.collection('personalchats');
+
+  
+
+  personalchats.doc(docID)
+      .collection('messages')
+      .add(chatMessageData);
+  personalchats.doc(docID)
+      .update({
     'recentMessage': chatMessageData['message'],
     'recentMessageSender': chatMessageData['sender'],
     'recentMessageTime': chatMessageData['time']
