92c63c1b1a5d18dcf2228ede7fec0851cd2e581d
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-11 08:09:46 +0530

    Add files via upload

---
+++ search_dialogs.dart
@@ -0,0 +1,73 @@
+import 'package:chat_app_project/firebase_services/database.dart';
+import 'package:flutter/material.dart';
+
+class UserSearchDelegate extends SearchDelegate {
+// Demo list to show querying
+  List<String> searchTerms = getAllUsers();
+
+// first overwrite to
+// clear the search text
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+// second overwrite to pop out of search menu
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+// third overwrite to show query result
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var u in searchTerms) {
+      if (u.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(u);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+// last overwrite to show the
+// querying process at the runtime
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var u in searchTerms) {
+      if (u.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(u);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
