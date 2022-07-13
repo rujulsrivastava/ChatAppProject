e54d5d8043d89c0062e9518da836ca81354209d7
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-13 18:12:07 +0530

    Update messaging.dart

--- messaging.dart
+++ messaging.dart
@@ -1,6 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:chat_app_project/customs/message_template.dart';
 
 sendMessageInGroup(String groupId, chatMessageData) {
   FirebaseFirestore.instance.collection('chatgroups').doc(groupId).collection('messages').add(chatMessageData);
@@ -24,12 +22,12 @@ sendMessageInChat(String senderID, String receiverID, String docID, chatMessageD
 
   if (senderChats.contains(docID) != true) {
     await senderDocRef.update({
-      'userchats': FieldValue.arrayUnion([docID])
+      'userchats': FieldValue.arrayUnion([receiverID])
     });
   }
   if (receiverChats.contains(docID) != true) {
     await receiverDocRef.update({
-      'userchats': FieldValue.arrayUnion([docID])
+      'userchats': FieldValue.arrayUnion([senderID])
     });
   }
 
@@ -43,47 +41,8 @@ sendMessageInChat(String senderID, String receiverID, String docID, chatMessageD
     'recentMessageSender': chatMessageData['sender'],
     'recentMessageTime': chatMessageData['time']
   });
-  // DocumentSnapshot d = await personalchats.doc(docID).get();
-  // if (d["peer1"] == null) {
-  //   await senderDocRef.update({
-  //     'peer1': senderDocSnapshot["userName"]
-  //   });
-  // }
-  // if (d["peer2"] == null) {
-  //   await senderDocRef.update({
-  //     'peer2': receiverDocSnapshot["userName"]
-  //   });
-  // }
+
 
 
 }
 
-// class Messaging {
-//   FirebaseAuth auth = FirebaseAuth.instance;
-//   final database = FirebaseFirestore.instance;
-//   late CollectionReference reference;
-//
-//   Future sendMessage(String message) async {
-//     final finalMessage = MessageTemplate(
-//         userID: auth.currentUser!.uid,
-//         userName: auth.currentUser!.displayName!,
-//         roomID: "public",
-//         messageType: MessageType.text,
-//         message: message.trim(),
-//         dateOfCreation: DateTime.now());
-//     try {
-//       reference = database.collection("chatgroups");
-//       await reference.add(finalMessage.convertToJson());
-//       print("Successfully sent");
-//     } catch (e) {
-//       print("Message not sent, ${e.toString()}");
-//     }
-//   }
-//
-//   Stream<QuerySnapshot> getMessage(){
-//     return database.collection("chatgroups")
-//         .orderBy('dateOfCreation')
-//         .snapshots();
-//   }
-//
-// }
