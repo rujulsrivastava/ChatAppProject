818c57158acd9ec7f8735a7eb319d63d46e0d544
Rujul Srivastava <71875214+rujulsrivastava@users.noreply.github.com>
2022-07-13 05:46:09 +0530

    Delete list_of_groups.dart

--- list_of_groups.dart
+++
@@ -1,7 +0,0 @@
-Map<int, Chat> groupsList = {1 : Chat(title: 'Foodies', description: 'Khana banana nahi khana pasand hai'), 2 : Chat(title: 'Booksters', description: 'For all the book lovers!')};
-
-class Chat {
-  late String title;
-  late String? description;
-  Chat({required this.title, this.description});
-}
